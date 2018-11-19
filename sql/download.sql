c93d173b3f5e3d70f32abd44dd490a5eb04c6e62
Genpeng <xgp1227@gmail.com>
2018-11-19 09:08:02 +0800

    [Add] 数据预处理和使用LightGBM建模

--- sql/download.sql
+++ sql/download.sql
@@ -1,6 +1,12 @@
 SELECT COUNT(*) FROM dw.dwd_pl_so_order_f
 WHERE creation_date >= '2018-01-01'
 
+SELECT COUNT(*) FROM dw.dwd_pl_so_order_f
+WHERE creation_date >= UNIX_TIMESTAMP('2018-01-01')
+
+SELECT DISTINCT item_wid
+FROM dw.dwd_pl_so_order_f
+
 -- 下载数据
 SELECT  creation_date, manage_org_wid, sales_region_wid, region_wid, 
         sales_cen_wid, customer_wid, sales_category_wid, item_wid, 
@@ -10,3 +16,25 @@ SELECT  creation_date, manage_org_wid, sales_region_wid, region_wid,
         return_qty, return_received_qty, return_flag
 FROM dw.dwd_pl_so_order_f
 WHERE creation_date >= '2018-01-01'
+ORDER BY creation_date ASC
+
+-- item_info
+SELECT DISTINCT item_wid, item_code, item_name, sales_category_wid, 
+                item_price, item_uom, productform, energyeffratiing, 
+                brand, frequencytype, sales_segment1_code, sales_segment1_name
+                sales_segment2_code
+FROM dw.dwd_pl_so_order_f
+
+-- sales
+SELECT  creation_date, sales_cen_wid, customer_wid, item_code, 
+        item_qty, return_qty
+FROM dw.dwd_pl_so_order_f
+WHERE creation_date >= '2018-01-01'
+ORDER BY creation_date ASC
+
+SELECT  SUBSTR(creation_date, 1, 10) AS creation_date, sales_cen_wid, customer_wid, item_code, 
+        SUM(item_qty) AS item_qty, SUM(CASE WHEN return_qty IS NULL THEN 0 ELSE return_qty END) as return_qty
+FROM dw.dwd_pl_so_order_f
+WHERE creation_date >= '2018-01-01'
+GROUP BY creation_date, sales_cen_wid, customer_wid, item_code
+ORDER BY creation_date ASC
---
+++ src/util.py
@@ -0,0 +1,27 @@
+# _*_ coding: utf-8 _*_
+"""
+Some auxiliary classes or functions.
+
+Author: StrongXGP (xgp1227@gmail.com)
+Date:	2018/11/16
+"""
+
+class Inspector:
+    def find_outliers(self, filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            lines = f.read().splitlines()[1:]
+        d, outliers = {}, []
+        for line in lines:
+            cols = line.split(',')
+            k, v = cols[0], cols[1]
+            if k in d:
+                if v != d[k]:
+                    outliers.append(k)
+            else:
+                d[k] = v
+        return outliers
+		
+
+def main():
+	inspector = Inspector()
+	print(inspector.find_outliers('../data/salescen-region.csv'))
