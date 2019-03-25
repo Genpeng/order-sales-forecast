b1c519a2438d947c60fca2505abe0544e57edf37
“Genpeng” <xgp1227@gmai.com>
2019-03-25 18:31:03 +0800

    [Modify] the structure of the project

---
+++ level1/decision_tree.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+
+"""
+Use decision tree to predict the order amounts of next two month.
+
+Author: Genpeng Xu (xgp1227@gmail.com)
+"""
+
---
+++ util/pandas_util.py
@@ -0,0 +1,34 @@
+# _*_ coding: utf-8 _*_
+
+"""
+Some useful utility functions about pandas.
+
+Author: Genpeng Xu (xgp1227@gmail.com)
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
+def row_normalization(df):
+    """Normalize each row of data."""
+    df_bak = df.copy()
+    rnames = list(df_bak.index)
+    scalers = dict()
+    for rn in rnames:
+        scaler = MinMaxScaler().fit(df_bak.loc[rn].values.reshape(-1, 1))
+        df_bak.loc[rn] = scaler.transform(df_bak.loc[rn].values.reshape(-1, 1)).ravel()
+        scalers[rn] = scaler
+    return df_bak, scalers
+
+
+def row_restore(df_normalized, scalers):
+    """Convert data back from normalized values."""
+    df_bak = df_normalized.copy()
+    rnames = list(df_bak.index)
+    for rn in rnames:
+        scaler = scalers[rn]
+        df_bak.loc[rn] = scaler.inverse_transform(df_bak.loc[rn].values.reshape(-1, 1)).ravel()
+    return df_bak
+
