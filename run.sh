4318416628f4bbe7eb0b5d08b7d1db524200f871
“Genpeng” <xgp1227@gmai.com>
2019-04-10 10:58:11 +0800

    [Modify] the structure of this project

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python -u level1_order.py -c configs/config_example.json > log/level1_order 2>&1 &
+nohup python -u level1_order.py -c configs/config_example.json > logs/level1_order.log 2>&1 &
