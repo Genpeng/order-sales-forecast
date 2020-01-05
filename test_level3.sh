ace0ebcc712b42cbbf445133bc06a7bc0d34a7d0
Genpeng Xu <xgp1227@gmai.com>
2020-01-05 13:47:09 +0800

    modify shell script

---
+++ test_level3.sh
@@ -0,0 +1,2 @@
+nohup python -u level3_order_predict_history.py -c configs/level3_order_predict_history_config.json > logs/level3_order_predict_history.log 2>&1 &
+nohup python -u level3_order_predict_future.py -c configs/level3_order_predict_future_config.json > logs/level3_order_predict_future.log 2>&1 &
