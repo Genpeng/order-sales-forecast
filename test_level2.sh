5751abf5e9e234f2df6bd7ce94913db9866f5b71
Genpeng <xgp1227@gmail.com>
2020-01-05 14:18:48 +0800

    modify shell script

--- test_level2.sh
+++ test_level2.sh
@@ -1,1 +1,1 @@
-nohup  python -u level2_order_predict_history.py -c configs/level2_order_predict_history_config.json > logs/level2_order_predict_history.log 2>&1 &
+nohup python -u level2_order_predict_history.py -c configs/level2_order_predict_history_config.json > logs/level2_order_predict_history.log 2>&1 &
