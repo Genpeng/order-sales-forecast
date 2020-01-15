bd148e4c4c9a31fc653487cd70afe1f92d33af89
Genpeng Xu <xgp1227@gmai.com>
2020-01-15 11:47:01 +0800

    modify predict future shell script

--- run_predict_future.sh
+++ run_predict_future.sh
@@ -1,4 +1,6 @@
-nohup python -u download_data.py > logs/download_data.log 2>&1 &
-nohup python -u level1_order_predict_future.py -c configs/level1_order_predict_future_config.json > logs/level1_order_predict_future.log 2>&1 &
-nohup python -u level2_order_predict_future.py -c configs/level2_order_predict_future_config.json > logs/level2_order_predict_future.log 2>&1 &
-nohup python -u level3_order_predict_future.py -c configs/level3_order_predict_future_config.json > logs/level3_order_predict_future.log 2>&1 &
+nohup python -u download_data.py > logs/predict_future.log 2>&1 &
+nohup python -u level1_order_predict_future.py -c configs/level1_order_predict_future_config.json > logs/predict_future.log 2>&1 &
+nohup python -u level2_order_predict_future.py -c configs/level2_order_predict_future_config.json > logs/predict_future.log 2>&1 &
+nohup python -u level3_order_predict_future.py -c configs/level3_order_predict_future_config.json > logs/predict_future.log 2>&1 &
+nohup python -u level3_dis_predict_future.py -c configs/level3_dis_predict_future_config.json > logs/predict_future.log 2>&1 &
+nohup python -u level3_inv_predict_future.py -c configs/level3_inv_predict_future_config.json > logs/predict_future.log 2>&1 &
