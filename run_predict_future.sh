7656d0fb922933747c95a94ffec7c5e35ae14574
Genpeng <xgp1227@gmail.com>
2020-01-16 13:38:22 +0800

    modify shell scripts

--- run_predict_future.sh
+++ run_predict_future.sh
@@ -1,6 +1,7 @@
-nohup python -u download_data.py > logs/predict_future.log 2>&1 &
-nohup python -u level1_order_predict_future.py -c configs/level1_order_predict_future_config.json > logs/predict_future.log 2>&1 &
-nohup python -u level2_order_predict_future.py -c configs/level2_order_predict_future_config.json > logs/predict_future.log 2>&1 &
-nohup python -u level3_order_predict_future.py -c configs/level3_order_predict_future_config.json > logs/predict_future.log 2>&1 &
-nohup python -u level3_dis_predict_future.py -c configs/level3_dis_predict_future_config.json > logs/predict_future.log 2>&1 &
-nohup python -u level3_inv_predict_future.py -c configs/level3_inv_predict_future_config.json > logs/predict_future.log 2>&1 &
+source activate xgp_py3
+python download_data.py
+python level1_order_predict_future.py -c configs/level1_order_predict_future_config.json
+python level2_order_predict_future.py -c configs/level2_order_predict_future_config.json
+python level3_order_predict_future.py -c configs/level3_order_predict_future_config.json
+python level3_dis_predict_future.py -c configs/level3_dis_predict_future_config.json
+python level3_inv_predict_future.py -c configs/level3_inv_predict_future_config.json
--- run_update_history.sh
+++ run_update_history.sh
@@ -1,6 +1,7 @@
-nohup python -u download_data.py > logs/update_history.log 2>&1 &
-nohup python -u level1_order_predict_history.py -c configs/level1_order_predict_history_config.json > logs/update_history.log 2>&1 &
-nohup python -u level2_order_predict_history.py -c configs/level2_order_predict_history_config.json > logs/update_history.log 2>&1 &
-nohup python -u level3_order_predict_history.py -c configs/level3_order_predict_history_config.json > logs/update_history.log 2>&1 &
-nohup python -u level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json > logs/update_history.log 2>&1 &
-nohup python -u level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json > logs/update_history.log 2>&1 &
+source activate xgp_py3
+python download_data.py
+python level1_order_predict_history.py -c configs/level1_order_predict_history_config.json
+nohup python level2_order_predict_history.py -c configs/level2_order_predict_history_config.json
+nohup python level3_order_predict_history.py -c configs/level3_order_predict_history_config.json
+nohup python level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json
+nohup python level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json
