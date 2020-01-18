fe6c6c0bb9e87bbeaf3aaf0b9978656c055e8e11
Genpeng <xgp1227@gmail.com>
2020-01-18 09:41:42 +0800

    modify update history script

--- run_update_history.sh
+++ run_update_history.sh
@@ -2,7 +2,7 @@
 source activate xgp_py3
 python download_data.py
 python level1_order_predict_history.py -c configs/level1_order_predict_history_config.json
-nohup python level2_order_predict_history.py -c configs/level2_order_predict_history_config.json
-nohup python level3_order_predict_history.py -c configs/level3_order_predict_history_config.json
-nohup python level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json
-nohup python level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json
+python level2_order_predict_history.py -c configs/level2_order_predict_history_config.json
+python level3_order_predict_history.py -c configs/level3_order_predict_history_config.json
+python level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json
+python level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json
