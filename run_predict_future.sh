c89269817a0d936900e5deb9bcc2f9a0d885382d
Genpeng Xu <xgp1227@gmai.com>
2020-02-21 12:18:39 +0800

    v1.0.0

--- run_predict_future.sh
+++ run_predict_future.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-source activate xgp_py3
-python download_data.py
-python level1_order_predict_future.py -c configs/level1_order_predict_future_config.json
-python level2_order_predict_future.py -c configs/level2_order_predict_future_config.json
-python level3_order_predict_future.py -c configs/level3_order_predict_future_config.json
-python level3_dis_predict_future.py -c configs/level3_dis_predict_future_config.json
-python level3_inv_predict_future.py -c configs/level3_inv_predict_future_config.json
+python level1_order_predict_future.py -c configs/level1_order_predict_future_config.json > logs/predict_future/level1_order_predict_future.log & python level2_order_predict_future.py -c configs/level2_order_predict_future_config.json > logs/predict_future/level2_order_predict_future.log & python level3_order_predict_future.py -c configs/level3_order_predict_future_config.json > logs/predict_future/level3_order_predict_future.log & python level3_dis_predict_future.py -c configs/level3_dis_predict_future_config.json > logs/predict_future/level3_dis_predict_future.log & python level3_inv_predict_future.py -c configs/level3_inv_predict_future_config.json > logs/predict_future/level3_inv_predict_future.log
--- run_update_history.sh
+++ run_update_history.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-source activate xgp_py3
-python download_data.py
-python level1_order_predict_history.py -c configs/level1_order_predict_history_config.json
-python level2_order_predict_history.py -c configs/level2_order_predict_history_config.json
-python level3_order_predict_history.py -c configs/level3_order_predict_history_config.json
-python level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json
-python level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json
+python level1_order_predict_history.py -c configs/level1_order_predict_history_config.json > logs/update_history/level1_order_predict_history.log & python level2_order_predict_history.py -c configs/level2_order_predict_history_config.json > logs/update_history/level2_order_predict_history.log & python level3_order_predict_history.py -c configs/level3_order_predict_history_config.json > logs/update_history/level3_order_predict_history.log & python level3_dis_predict_history.py -c configs/level3_dis_predict_history_config.json > logs/update_history/level3_dis_predict_history.log & python level3_inv_predict_history.py -c configs/level3_inv_predict_history_config.json > logs/update_history/level3_inv_predict_history.log &
